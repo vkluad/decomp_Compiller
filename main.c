78aafecda03c5a2a69f9f0c2982a5c7b11c8500c
vkluad <vkluad@gmail.com>
2021-05-06 16:18:14 +0300

    update from linux

--- main.c
+++ main.c
@@ -6,10 +6,11 @@ int main(int argc, char *argv[]) {
     char filename[FILENAME_SIZE] = "";
     char new_filename[FILENAME_SIZE] = "";
     if(argc<2){
+
         printf("Please enter the file name(q - quit):\n");
+        scanf("%s",filename);
         if(strcmp(filename,"q")==0)
             return 0;
-        scanf("%s",filename);
         pre_reader(filename);
         strncpy(new_filename,filename, strlen(filename));
         strcat(new_filename,"_pre_comp");
