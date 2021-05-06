29b5f75de9a883ae682311ed3ed17cfbd03ea0fa
vkluad <vkluad@gmail.com>
2021-05-06 14:53:24 +0300

    update for linux

---
+++ cmake-build-debug/CMakeFiles/decomp_Compiller.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/decomp_Compiller.dir/compiler.c.o"
+  "CMakeFiles/decomp_Compiller.dir/main.c.o"
+  "CMakeFiles/decomp_Compiller.dir/pre_reader.c.o"
+  "decomp_Compiller"
+  "decomp_Compiller.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/decomp_Compiller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
