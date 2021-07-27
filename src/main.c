73d5888105d7b854df53b1d8e31050c884f89b0d
Vlad Kudenchuk <vkluad@gmail.com>
2021-07-27 20:59:48 +0300

    repo: clang format( linux kernel coding style
    Signed-off-by: Vlad Kudenchuk <vkluad@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,25 +1,26 @@
 #include "compiler.h"
 #include "pre_reader.h"
 
-int main(int argc, char *argv[]) {
-  char filename[FILENAME_SIZE] = "";
-  char new_filename[FILENAME_SIZE] = "";
+int main(int argc, char *argv[])
+{
+	char filename[FILENAME_SIZE] = "";
+	char new_filename[FILENAME_SIZE] = "";
 
-  if (argc < 2) {
-    printf("Please enter the file name(q - quit):\n");
-    scanf("%s", filename);
-    if (strcmp(filename, "q") == 0)
-      return 0;
-  } else {
-    strcpy(filename, argv[1]);
-  }
-  if (pre_reader(filename) == 0) {
-    strncpy(new_filename, filename, strlen(filename));
-    strcat(new_filename, "_pre_comp");
-    compiler(new_filename);
-    load_to_decomp(filename);
-  } else {
-    return 1;
-  }
-  return 0;
+	if (argc < 2) {
+		printf("Please enter the file name(q - quit):\n");
+		scanf("%s", filename);
+		if (strcmp(filename, "q") == 0)
+			return 0;
+	} else {
+		strcpy(filename, argv[1]);
+	}
+	if (pre_reader(filename) == 0) {
+		strncpy(new_filename, filename, strlen(filename));
+		strcat(new_filename, "_pre_comp");
+		compiler(new_filename);
+		load_to_decomp(filename);
+	} else {
+		return 1;
+	}
+	return 0;
 }
